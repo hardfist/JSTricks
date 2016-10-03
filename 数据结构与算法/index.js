d0bb1cc7850e390c273c7bd0845c6625cd74d4e0
shengju.yj <shengju.yj@alibaba-inc.com>
2016-10-03 22:45:29 +0800

    change jsdoc

--- 数据结构与算法/index.js
+++ 数据结构与算法/index.js
@@ -4,7 +4,9 @@
  * @param hi
  * @param step
  * @returns {Array}
- * @sample range(1,2,10) => [1,3,5,7,9]
+ * @example
+ * // returns [1,3,5,7,9]
+ * range(1,2,10)
  */
 function range(lo,hi,step=1){
     return Array.from({length:hi-lo},(_,i) => lo+i*step)
@@ -15,7 +17,9 @@ function range(lo,hi,step=1){
  * @param right
  * @param fn
  * @returns {Array}
- * @sample zip(['a','b','c'],[1,2,3]) => [['a',1],['b',2],['c',3]]
+ * @example
+ * // returns [['a',1],['b',2],['c',3]]
+ * zip(['a','b','c'],[1,2,3])
  */
 function zip(left,right,fn=(l,r) => [l,r]){
     var counter, results = [];
@@ -29,7 +33,9 @@ function zip(left,right,fn=(l,r) => [l,r]){
  * @param keys
  * @param values
  * @returns {Map}
- * @sample arrays2dict(['a','b','c'],[1,2,3]) => Map(a=>1,b=>2,c=>3)
+ * @example
+ * // returns  Map(a=>1,b=>2,c=>3)
+ * arrays2dict(['a','b','c'],[1,2,3]) =>
  */
 function arrays2dict(keys,values){
     return new Map(zip(keys,values));
@@ -39,7 +45,9 @@ function arrays2dict(keys,values){
  * @param dict
  * @param fn
  * @returns {Map}
- * @sample filterValue({a=>1,b=>2},x => x=='b') => {b=>2}
+ * @example
+ * // return {b=>2}
+ * filterValue({a=>1,b=>2},x => x=='b')
  */
 function filterKey(dict,fn){
     let result = new Map();
@@ -55,7 +63,9 @@ function filterKey(dict,fn){
  * @param dict
  * @param fn
  * @returns {Map}
- * @sample filterValue({a=>1,b=>2},x => x==2) => {b=>2}
+ * @example
+ * // returns {b => 2}
+ * filterValue({a=>1,b=>2},x => x==2)
  */
 function filterValue(dict,fn){
     let result = new Map();
@@ -71,7 +81,9 @@ function filterValue(dict,fn){
  * @param s
  * @param fn
  * @returns {Set}
- * @sample filterSet({1,2,3},x => x%2 ==0) => {2}
+ * @example
+ * // returns {2}
+ * filterSet({1,2,3},x => x%2 ==0)
  */
 function filterSet(s,fn){
     let result = new Set();
