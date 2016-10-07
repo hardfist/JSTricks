7646e66112505fb736fec3cbb8b81982c35a4c6d
hardfist <yangjianzju@gmail.com>
2016-10-07 15:50:34 +0800

    finish LL(1) parser

--- ch5/index.js
+++ ch5/index.js
@@ -1,64 +1,109 @@
-const COMMA = Symbol('COMMA');
-const LBRACK = Symbol('LBRACK');
-const RBRACK = Symbol('RBRACK');
-const NAME = Symbol('NAME');
-const EOF = Symbol('EOF');
 class Token{
     constructor(type,text){
         this.type = type;
         this.text = text;
     }
     toString(){
-        return `<${this.type},${this.text}>`;
+        return `<${String(this.type)},${this.text}>`;
     }
 }
 class Lexer{
     constructor(input){
         this.input = input.split('');
+        this.consume();//初始化
     }
     consume(){
         this.c = this.input.shift();
     }
+    match(x){
+        if(this.c == x) this.consume();
+        throw new TypeError(`expecting ${x} but found ${this.c}`);
+    }
+    getTokens(){
+        let token = this.nextToken();
+        let tokens = [];
+        while(token != null){
+            tokens.push(token);
+            token = this.nextToken();
+        }
+        return tokens;
+    }
+}
+class ListLexer extends Lexer{
     nextToken(){
-        this.consume();
         while(this.c!=null){
-            while(/\s/.test(this.c)) this.consume();
-            if(this.c == null) break;
+            if(/s/.test(this.c)){
+                while(/s/.test(this.c)){
+                    this.consume()
+                }
+                continue;
+            }
             switch (this.c){
-                case ',': this.consume();return new Token(COMMA,',');
-                case '[': this.consume(); return new Token(LBRACK,'[');
-                case ']': this.consume(); return new Token(RBRACK,']');
+                case ',': this.consume();return new Token(ListLexer.COMMA,',');
+                case '[': this.consume(); return new Token(ListLexer.LBRACK,'[');
+                case ']': this.consume(); return new Token(ListLexer.RBRACK,']');
                 default: {
                     if (/[a-zA-Z]/.test(this.c)) {
                         let name  = '';
-                        while(/a-zA-Z/.test(this.c)){
+                        while(/[a-zA-Z]/.test(this.c)){
                             name += this.c;
                             this.consume()
                         }
-                        return new Token(NAME,name);
+                        return new Token(ListLexer.NAME,name);
                     }
                     throw new TypeError('Invalid Characters:', this.c);
                 }
             }
         }
-        return new Token(EOF,'<EOF>');
+        return null;
     }
 }
+ListLexer.COMMA = Symbol('COMMA');
+ListLexer.LBRACK = Symbol('LBRACK');
+ListLexer.RBRACK = Symbol('RBRACK');
+ListLexer.NAME = Symbol('NAME');
+ListLexer.EOF = Symbol('EOF');
+
 class Parser{
-    constructor(input){
-        this.input = input;
+    constructor(tokens){
+        this.input = tokens;
+        this.consume()
     }
-    getTokens(){
-        let lexer = new Lexer(this.input);
-        let token = lexer.nextToken();
-        let tokens = [];
-        while(token.type != EOF){
-            tokens.push(token);
-            token = lexer.nextToken();
+    match(type){
+        if(this.token.type === type) this.consume();
+        else{
+           throw new TypeError(`expect ${String(type)} but get ${String(this.token.type)}`);
         }
-        return tokens;
+    }
+    consume(){
+        this.token = this.input.shift();
     }
 }
-let parser = new Parser('[ a , b ]');
-let tokens = parser.getTokens();
-console.log('tokens:',tokens);
+class ListParser extends Parser{
+    constructor(input){
+        super(input);
+    }
+    list(){
+        this.match(ListLexer.LBRACK);
+        this.elements();
+        this.match(ListLexer.RBRACK);
+    }
+    elements(){
+        this.element();
+        while(this.token.type === ListLexer.COMMA){
+            this.match(ListLexer.COMMA);
+            this.element()
+        }
+    }
+    element(){
+        if(this.token.type === ListLexer.NAME) this.match(ListLexer.NAME);
+        else if(this.token.type === ListLexer.LBRACK) this.list();
+        else throw new TypeError(`expecting name or list: found ${this.token}`);
+    }
+}
+module.exports={
+    Lexer,
+    ListLexer,
+    Parser,
+    ListParser
+};
